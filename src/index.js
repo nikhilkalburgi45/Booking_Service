3e64af504561cb268631c0126c791ec49d26c5a2
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-04-29 12:41:31 +0530

    feat: Added Basic Route Setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const {PORT} = require('./config/serverConfig')
+const apiRoutes = require('./routes/index')
+
+const db = require('./models/index')
 
 
 const setupAndStartServer = () => {
@@ -10,8 +13,18 @@ const setupAndStartServer = () => {
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+app.use('/api',apiRoutes)
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
+  if(process.env.DB_SYNC){
+    db.sequelize.sync({alter:true})
+  }
+
+
 });
 
 }
