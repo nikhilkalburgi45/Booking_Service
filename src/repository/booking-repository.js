4e52a36fc5a4fe39b7fcf89f7bc701d164c3db53
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-05-13 09:54:37 +0530

    feat(repository): implement booking repository with create and update operations

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -1,26 +1,43 @@
-const {Booking} = require('../models/index');
-const { AppError } = require('../utils/errors');
-const {ValidationError,AppError} = require('../utils/errors/valideation-error');
-const {StatusCodes} = require('http-status-codes');
+const { Booking } = require("../models/index");
+const { AppError } = require("../utils/errors");
+const { ValidationError } = require("../utils/errors/valideation-error");
+const { StatusCodes } = require("http-status-codes");
 
-
-class BookingRepository{
-
-    async create(data){
-        try{
-            const booking = await Booking.create(data);
-            return booking;
-        }catch(error){
-            if(error.name == 'SequelizeValidationError'){
-                throw new ValidationError(error);
-            }
-            throw new AppError('Repository Error', 
-                'Cannot create booking', 
-                'There might be a problem with the data sent in the request', 
-                StatusCodes.INTERNAL_SERVER_ERROR);
-        }
+class BookingRepository {
+  async create(data) {
+    try {
+      const booking = await Booking.create(data);
+      return booking;
+    } catch (error) {
+      if (error.name == "SequelizeValidationError") {
+        throw new ValidationError(error);
+      }
+      throw new AppError(
+        "Repository Error",
+        "Cannot create booking",
+        "There might be a problem with the data sent in the request",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
     }
-}   
+  }
+
+  async update(bookingId, data) {
+    try {
+      const booking = await Booking.findByPk(bookingId);
+      if (data.status) {
+        booking.status = data.status;
+      }
+      await booking.save();
+      return booking;
+    } catch (error) {
+      throw new AppError(
+        "Repository Error",
+        "Cannot update booking",
+        "There might be a problem with the data sent in the request",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+}
 
 module.exports = BookingRepository;
-
