ad3b270822c5aa3adc9feb37d8d7f1e53a2d1944
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-05-13 09:56:29 +0530

    feat(routes): configure booking routes in v1

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
+const express = require("express");
+
+const { bookingController } = require("../../controllers/index");
 
 const router = express.Router();
 
-
-
+router.post("/bookings", bookingController.create);
 
 module.exports = router;
